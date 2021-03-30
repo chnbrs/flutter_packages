6d4ada1c993f63b045092239aa9fec6d69ce5d3e
chnbrs <chnbrs@gmail.com>
2021-03-30 06:52:35 +0300

    Create email_password_sign_in_strings.dart

---
+++ email_password_sign_in_strings.dart
@@ -0,0 +1,29 @@
+part of email_password_sign_in_ui;
+
+class EmailPasswordSignInStrings {
+  static const String ok = 'Tamam';
+  // Email & Password page
+  static const String signIn = 'Giriş Yap';
+  static const String register = 'Üye Ol';
+  static const String forgotPassword = 'Şifremi Unuttum';
+  static const String forgotPasswordQuestion = 'Şifrenizi mi unuttunuz?';
+  static const String createAnAccount = 'Hesap oluştur';
+  static const String needAnAccount = 'Hesabınız yok mu? Üye ol';
+  static const String haveAnAccount = 'Hesabınız var mı? Giriş Yapın';
+  static const String signInFailed = 'Giriş Başarısız';
+  static const String registrationFailed = 'Üyelik başarısız';
+  static const String passwordResetFailed = 'Şifre sıfırlama başarısız.';
+  static const String sendResetLink = 'Sıfırlama bağlantısı gönder';
+  static const String backToSignIn = 'Geri';
+  static const String resetLinkSentTitle = 'Sıfırlama linki gönderildi';
+  static const String resetLinkSentMessage =
+      'Hesabınızı sıfırlamak için e-posta kutunuzu kontrol edin';
+  static const String emailLabel = 'E-Posta';
+  static const String emailHint = 'birisi@mailservisi.com';
+  static const String password8CharactersLabel = 'Şifre 7 karakterden fazla olmalı';
+  static const String passwordLabel = 'Şifre';
+  static const String invalidEmailErrorText = 'Geçersiz E-posta';
+  static const String invalidEmailEmpty = 'E-posta boş bırakılamaz';
+  static const String invalidPasswordTooShort = 'Şifre çok kısa';
+  static const String invalidPasswordEmpty = 'Şifre boş bırakılamaz';
+}
